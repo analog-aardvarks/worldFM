ec3582c4f12bf2bef80debfde70691ea2a55d8f5
Hack Reactor Student <student@hackreactor.com>
2017-05-19 16:08:39 -0400

    [refactor] refactored to remove all inline styles

--- src/public/app/components/Song.jsx
+++ src/public/app/components/Song.jsx
@@ -17,40 +17,10 @@ const Song = props => {
         }}
       >
         <div className="Song__info">
-          <span
-            className="Song__name"
-            style={{
-              fontFamily: '\'Archivo Black\', sans-serif',
-              fontSize: '15px',
-              color: 'white',
-              backgroundColor: 'rgba(15, 15, 15, 0.35)',
-              display: 'block',
-            }}>
-            {props.name}
-          </span>
-          <span
-            className="Song__artist"
-            style={{
-              fontFamily: '\'Archivo Black\', sans-serif',
-              fontSize: '15px',
-              color: 'white',
-              display: 'block',
-              backgroundColor: 'rgba(15, 15, 15, 0.35)',
-            }}>
-            {props.artists[0].name}
-          </span>
+          <span className="Song__name">{props.name}</span>
+          <span className="Song__artist">{props.artists[0].name}</span>
         </div>
-        <span
-          className="Song__artist"
-          style={{
-            fontFamily: '\'Archivo Black\', sans-serif',
-            fontWeight: '900',
-            fontSize: '30px',
-            color: 'white',
-            alignSelf: 'flex-end',
-          }}>
-          {props.ranking}
-        </span>
+        <span className="Song__ranking">{props.ranking}</span>
       </div>
     );
 }
