4ebad4cfd8ef983a9ceb73456f2c4953b984fe82
Arturo Ruvalcaba <a.ruvham@gmail.com>
2017-05-19 14:54:19 -0400

    [created] first iteration of landing page

---
+++ src/public/app/components/Playlist.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import Song from './Song.jsx';
+import samplePlaylist from './../config/samplePlaylist.jsx';
+
+const NAVBAR_HEIGHT = 0; // px
+const PLAYER_HEIGHT = 0; // px
+const PLAYLIST_HEIGHT = window.innerHeight - NAVBAR_HEIGHT - PLAYER_HEIGHT;
+
+class Playlist extends React.Component {
+  render() {
+    return (
+      <div
+        className="Playlist"
+        style={{
+          height: PLAYLIST_HEIGHT
+        }}
+      >
+        {samplePlaylist && samplePlaylist.items.map((listItem) => {
+          return (
+            <Song
+              key={listItem.track.id}
+              name={listItem.track.name}
+              artists={listItem.track.artists}
+              album={listItem.track.album}
+              size={PLAYLIST_HEIGHT / 3}
+            />
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+export default Playlist;
--- src/public/app/config/routes.jsx
+++ src/public/app/config/routes.jsx
@@ -1,21 +1,20 @@
 import React from 'react';
 import { Route, Link } from 'react-router-dom';
+import Playlist from './../components/Playlist.jsx'
 
-const Landing = () => <h1>Welcome to the Landing Page</h1>;
-const World = props => <h1>Welcome to the {props.match.params.data} Page</h1>;
+const Landing = () => (
+  <div>
+    <h1>Welcome to the Landing Page</h1>
+    <ul>
+      <li><Link to="/Playlist">Playlist Component</Link></li>
+    </ul>
+  </div>
+);
 
 const routes = (
   <div>
     <Route exact path="/" component={Landing} />
-    <Route path="/:data" component={World} />
-
-    <ul>
-      <li><Link to="/Spain">Spain</Link></li>
-      <li><Link to="/Italy">Italy</Link></li>
-      <li><Link to="/Germany">Germany</Link></li>
-    </ul>
-
-    <p>Try typing a different country directly on the URL!</p>
+    <Route exact path="/playlist" component={Playlist} />
   </div>
 );
 
