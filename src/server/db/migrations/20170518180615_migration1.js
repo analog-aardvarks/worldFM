923d0d0dfc25e217fe567aa5249ac457204d0390
Arturo Ruvalcaba <a.ruvham@gmail.com>
2017-05-20 19:56:48 -0400

    [update] update migration to include necessary information

--- src/server/db/migrations/20170518180615_migration1.js
+++ src/server/db/migrations/20170518180615_migration1.js
@@ -1,21 +1,25 @@
 exports.up = function(knex, Promise) {
   return Promise.all([
-    knex.schema.createTableIfNotExists('playlists', function (table) {
-      table.string('name');
-      table.string('playlistId');
-      table.integer('tracks');
-    }).then(function () {
-      console.log('created playlists table!')
+    knex.schema.createTableIfNotExists('playlists', function(table) {
+      table.string( 'playlist_name');
+      table.string( 'playlist_id');
+      table.integer('playlist_tracks_total');
+    })
+    .then(function () {
+      console.log('table \'playlist\' successfully created!');
     }),
-    knex.schema.createTableIfNotExists('songs', function (table) {
-      table.string('songName');
-      table.string('artistName');
-      table.string('previewUrl');
-      table.string('albumArt');
-      table.string('songId');
-      table.string('playlistId');
-    }).then(function (){
-      console.log('created songs tables!')
+
+    knex.schema.createTableIfNotExists('songs', function(table) {
+      table.string('track_name');
+      table.string('track_id');
+      table.string('track_preview_url');
+      table.string('track_album_id');
+      table.string('track_album_image');
+      table.string('track_artist_name');
+      table.string('track_playlist_id');
+    })
+    .then(function (){
+      console.log('table \'songs\' successfully created!');
     })
   ]);
 };
