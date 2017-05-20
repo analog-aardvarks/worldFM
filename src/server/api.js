f8477e298f89f445306444710b8dc6ae0be97117
David H <davidhamberlin@gmail.com>
2017-05-20 12:13:15 -0400

    replace removed lines, fix missing dependencies in package.json, move db directory

--- src/server/api.js
+++ src/server/api.js
@@ -1,4 +1,3 @@
-const server = require('./server');
 const fetch = require('node-fetch');
 const auth = require('./auth');
 // const token =
@@ -6,10 +5,10 @@ const auth = require('./auth');
 module.exports.getPlaylist = (req, res) => {
   fetch('https://api.spotify.com/v1/users/thesoundsofspotify/playlists/4LbFHmTvu6bQldLAiCQ8KF', {
     dataType: 'json',
-    headers: `Authorization: Bearer ${auth.accessToken}`
+    headers: `Authorization: Bearer ${auth.accessToken}`,
   })
     .then((response) => {
-      console.log('fetched: ', response);
+      // console.log('fetched: ', response);
       res.send(JSON.stringify(response), null, 4);
     });
 };
--- src/server/server.js
+++ src/server/server.js
@@ -1,26 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = new express();
+const path = require('path');
 const morgan = require('morgan');
 const session = require('express-session');
 const passport = require('passport');
 
 const config = require('../../config');
-const auth = require('./auth');
 const routes = require('./routes');
+
+const app = express();
 
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(session({ secret: config.seshSecret }))
+app.use(session({ secret: config.seshSecret }));
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(express.static(path.join(__dirname, '../public')));
 app.use(routes);
-
-// app.get('/', (req, res) => res.send('Sup, World'));
 
 
 const port = process.env.port || 8080;
-const server = app.listen(port, () => {
+app.listen(port, () => {
   console.log(`Listening on port ${port}`)
-})
+});
