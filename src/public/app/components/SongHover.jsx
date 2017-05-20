254ca3ef8edd23761bc51629121f2d1a5d89bf43
Hack Reactor Student <student@hackreactor.com>
2017-05-19 20:09:45 -0400

    [add] SongHover component and hover buttons over Song

---
+++ src/public/app/components/SongHover.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SongHover = () => {
+  return (
+    <div className="SongHover">
+      <i className="SongHover__play-button fa fa-play-circle-o fa-5x fa-fw"></i>
+    </div>
+  )
+}
+
+export default SongHover;
