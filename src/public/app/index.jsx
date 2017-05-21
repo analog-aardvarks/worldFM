217cbdb86564a5a17f55f73c7f7c8d49c17d1725
David H <davidhamberlin@gmail.com>
2017-05-21 12:44:16 -0400

    rejigger file structure

--- src/public/app/index.jsx
+++ src/public/app/index.jsx
@@ -2,14 +2,21 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import Routes from './containers/Routes.jsx';
+import { HashRouter, Route, Link } from 'react-router-dom';
+import App from './components/App.jsx'
+import Landing from './components/Landing.jsx';
 import reducer from './reducers';
 
 let store = createStore(reducer);
 
 render(
   <Provider store={store}>
-    <Routes />
+    <HashRouter>
+      <div>
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/playlist" component={App} />
+      </div>
+    </HashRouter>
   </Provider>,
   document.getElementById('app')
 );
