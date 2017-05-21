b540ade3070b0c63e044c850026f0ada52f5874d
Arturo Ruvalcaba <a.ruvham@gmail.com>
2017-05-21 19:58:26 -0400

    [add] documentation to worker.js

--- src/server/worker.js
+++ src/server/worker.js
@@ -1,12 +1,16 @@
 const SpotifyWebApi = require('spotify-web-api-node');
+const config = require('../../config');
+
 const Track = require('./db/models/Track.js');
 const Playlist = require('./db/models/Playlist.js');
-const config = require('../../config');
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Auth
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+// Instructions:
+// Get your own token from https://developer.spotify.com/web-api/console/
+// Add it to the config.js file as a property named 'token'
 const spotifyApi = new SpotifyWebApi({
   clientId : config.clientId,
   clientSecret : config.clientSecret,
@@ -17,6 +21,7 @@ spotifyApi.setAccessToken(config.token);
 // Helpers
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+// Probably unnecessary with some refactoring of the worker function
 const promisifyGetPlaylist = function(owner, id) {
   return new Promise((resolve, reject) => {
     spotifyApi.getPlaylist(owner, id)
@@ -29,6 +34,7 @@ const promisifyGetPlaylist = function(owner, id) {
   });
 }
 
+// Parse raw data to be in line with schema structure
 const parseTrackData = function(trackData) {
   return {
     track_id: trackData.track.id,
@@ -40,6 +46,7 @@ const parseTrackData = function(trackData) {
   }
 }
 
+// Parse raw data to be in line with schema structure
 const parsePlaylistData = function(playlistData, tracksArray) {
   return {
     playlist_id: playlistData.id,
@@ -53,29 +60,26 @@ const parsePlaylistData = function(playlistData, tracksArray) {
 // Worker
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+// Read 'Run' for details
+
 const spotifyWorker = function(owner, limit, offset) {
   spotifyApi.getUserPlaylists(owner, { limit: limit, offset: offset })
     .then(data => {
       data.body.items.forEach(playlistData => {
-
         const p = promisifyGetPlaylist(owner, playlistData.id);
         p.then(tracksData => {
-
           const tracksArray = [];
           tracksData.forEach(singleTrackData => {
-
             tracksArray.push(singleTrackData.track.id);
             const parsedTrackData = parseTrackData(singleTrackData);
-            // save data!
-            // console.log(parsedTrackData)
+            // Save track in database
             Track.postTrack(parsedTrackData);
           })
           return tracksArray;
         })
         .then(tracksArray => {
            const parsedPlaylistData = parsePlaylistData(playlistData, tracksArray);
-           // console.log(parsedPlaylistData);
-           // save data!
+           // Save playlist in database
            Playlist.postPlaylist(parsedPlaylistData);
         })
         .catch(err => console.log(err));
@@ -88,18 +92,21 @@ const spotifyWorker = function(owner, limit, offset) {
 // Main
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+// Read 'Run' for details
+
 const runWorkers = function() {
-  // attemps to get relevant playlists!
   spotifyWorker('thesoundsofspotify', 50,   0);
   spotifyWorker('thesoundsofspotify', 50,  50);
   spotifyWorker('thesoundsofspotify', 50, 100);
   spotifyWorker('thesoundsofspotify', 50, 150);
   spotifyWorker('thesoundsofspotify',  2, 200);
 
-  // EXPERIMENTAL, attemps to get all 9000+ playlists!
+  // EXPERIMENTAL! (not tested)
+  // Attemps to get all 9928 playlists from user 'thesoundsofspotify'
+  // Thousands of API calls, exceeds current call limit
 
   // for(var i = 0; i <= 10000; i += 50) {
-  //   spotifyWorker('thesoundsofspotify', 50,   i);
+  //   spotifyWorker('thesoundsofspotify', 50, i);
   // }
 }
 
@@ -107,9 +114,18 @@ const runWorkers = function() {
 // Run!
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-// TESTS
-// uncomment and include id, limit, offset
+// Worker function:
+// Gets a number of playlists created by specified user, then attemps to save it in database
+// Then iterates through the first 100 songs in the playlist and attemps to save them in database
+// Params: owner, limit, offset
+
+// const owner = 'thesoundsofspotify';
+// const limit = 5;
+// const offset = 0
 // spotifyWorker('thesoundsofspotify', 1, 0);
 
-// WARNING! Hundreds of API calls, run with caution!
+// Run function:
+// Attemps to get the first 202 playlists from user 'thesoundsofspotify'
+// WARNING! Hundreds of API calls, run with caution as call limit can be reached fast
+
 // runWorkers();
