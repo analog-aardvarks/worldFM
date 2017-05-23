2f49761c40f141714b352122cdb4eb66eb19431f
Anthony Greenheck <aemgreenheck@gmail.com>
2017-05-22 20:05:28 -0400

    [added] some responsiveness

--- src/public/app/components/Songs.jsx
+++ src/public/app/components/Songs.jsx
@@ -2,26 +2,47 @@ import React from 'react';
 import Song from './Song.jsx';
 import Menu from './Menu.jsx';
 import { PLAYLIST_HEIGHT } from '../config/dimensions';
+import { PLAYLIST_WIDTH } from '../config/dimensions';
 
-const Songs = ({ playlist, onClick }) => (
-  <div
-    className="Playlist"
-    style={{
-      height: PLAYLIST_HEIGHT,
-    }}
-  >
-    {
-      playlist.map((listItem, idx) => (
-      <Song
-        key={listItem.track_id}
-        ranking={idx + 1}
-        track={listItem}
-        size={PLAYLIST_HEIGHT / 3}
-        onClick={onClick}
-      />
-    ))}
+const Songs = ({ playlist, onClick }) => {
+  let width = 0;
+  if (PLAYLIST_WIDTH < 500) {
+    width = PLAYLIST_WIDTH / 2;
+  }
+  else if (PLAYLIST_WIDTH < 800) {
+    width = PLAYLIST_WIDTH / 3;
+  }
+  else if (PLAYLIST_WIDTH < 1100) {
+    width = PLAYLIST_WIDTH / 4;
+  }
+  else if (PLAYLIST_WIDTH < 1450) {
+    width = PLAYLIST_WIDTH / 5;
+  }
+  else  {
+    width = PLAYLIST_WIDTH / 6;
+  }
 
-  </div>
-);
+  return (
+    <div
+      className="Playlist"
+      style={{
+        // height: PLAYLIST_HEIGHT,
+        width: PLAYLIST_WIDTH,
+      }}
+    >
+      {
+        playlist.map((listItem, idx) => (
+        <Song
+          key={listItem.track_id}
+          ranking={idx + 1}
+          track={listItem}
+          size={width}
+          onClick={onClick}
+        />
+      ))}
+
+    </div>
+  );
+}
 
 export default Songs;
