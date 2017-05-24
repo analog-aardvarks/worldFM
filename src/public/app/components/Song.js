e879166252ddf8f144b5ad378bd3aeee000cfddd
Arturo Ruvalcaba <a.ruvham@gmail.com>
2017-05-24 13:32:35 -0400

    [update] song component and toggle track info refactor

--- src/public/app/components/Song.js
+++ src/public/app/components/Song.js
@@ -1,30 +1,28 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import SongHover from './SongHover'
-
-const TOTAL_MARGINS = 2; // px
 
 const Song = ({ size, track, onClick, ranking, currentSong, showTrackInfo }) => {
-  const netSize = size - TOTAL_MARGINS;
-  // console.log(track);
+
+  const borderWidth = 3; // px
+  const netSize = size - borderWidth;
+  const icon = currentSong.isPlaying && track.track_preview_url === currentSong.src ? 'fa-pause-circle-o' : 'fa-play-circle-o';
+
   return (
-    <div>
+    <div
+      className="Song"
+      onClick={() => onClick(track.track_preview_url)}
+      style={{
+        backgroundImage: `url(${track.track_album_image})`,
+        width: netSize,
+        height: netSize,
+      }}
+    >
+
       <div
-        onClick={() => onClick(track.track_preview_url)}
-        className="Song"
+        className="Song--wrapper"
         style={{
-          backgroundImage: `url(${track.track_album_image})`,
-          minWidth: netSize,
-          maxWidth: netSize,
-          minHeight: netSize,
-          maxHeight: netSize
+          opacity: showTrackInfo ? 1 : 0,
         }}
       >
-        <SongHover
-          currentSong={currentSong}
-          track={track}
-        />
-        { showTrackInfo &&
         <div className="Song__container">
           <span className="Song__ranking">{ranking < 10 ? `0${ranking}` : ranking}</span>
           <div className="Song__info">
@@ -35,10 +33,34 @@ const Song = ({ size, track, onClick, ranking, currentSong, showTrackInfo }) => 
             <i className="fa fa-chevron-circle-up fa-2x fa-fw" />
           </span>
         </div>
-        }
+      </div>
+
+      <div
+        className="SongHover"
+        style={{ bottom: netSize - (2 * borderWidth) }}
+      >
+        <i className={`SongHover__play-button fa ${icon} fa-5x fa-fw`}></i>
       </div>
     </div>
   );
 }
+
+/*
+<div
+  className="Song--wrapper"
+  style={{ bottom: netSize - (2 * borderWidth) }}
+>
+  <div className="Song__container">
+    <span className="Song__ranking">{ranking < 10 ? `0${ranking}` : ranking}</span>
+    <div className="Song__info">
+      <span className="Song__name">{track.track_name}</span>
+      <span className="Song__artist">{JSON.parse(track.track_artist_name).join(', ')}</span>
+    </div>
+    <span className="Song__expand">
+      <i className="fa fa-chevron-circle-up fa-2x fa-fw" />
+    </span>
+  </div>
+</div>
+*/
 
 export default Song;
