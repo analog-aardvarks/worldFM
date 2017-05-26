df00bac2105ed87b3b29374bed63456d740f6199
Arturo Ruvalcaba <a.ruvham@gmail.com>
2017-05-26 00:56:23 -0400

    [create] created API routes for controlling spotify's playback

---
+++ src/server/helpers/Devices.js
@@ -0,0 +1,16 @@
+const rpn = require('request-promise-native');
+
+const token = '';
+const Devices = {};
+
+Devices.info = (req, res) => {
+  rpn({
+    method: 'GET',
+    url: 'https://api.spotify.com/v1/me/player/devices',
+    headers: { Authorization: `Bearer ${token}` },
+  })
+    .then(response => res.status(300).send('Success!', response))
+    .catch(err => res.status(404).send(err));
+};
+
+module.exports = Devices;
