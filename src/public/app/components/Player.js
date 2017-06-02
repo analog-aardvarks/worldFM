650f8a8090ebf4b3b533e217cae1e7c6f69d0378
Arturo Ruvalcaba <a.ruvham@gmail.com>
2017-06-02 00:10:14 -0400

    [feat] add icon to add playlist to spotify

--- src/public/app/components/Player.js
+++ src/public/app/components/Player.js
@@ -22,6 +22,7 @@ const mapStateToProps = state => ({
   spotifyPlayer: state.spotifyPlayer,
   showVolumeGauge: state.showVolumeGauge,
   showAvailableDevices: state.showAvailableDevices,
+  currentCountry: state.currentCountry,
 });
 
 const mapDispatchToProps = dispatch => ({
@@ -228,6 +229,27 @@ class Player extends React.Component {
         <div className="PlayerControls">
 
           <div className="PlayerControlsPlay">
+            <span
+              className="fa fa fa-futbol-o fa-2x fa-fw"
+              style={{ color: 'rgb(255,0,255)' }}
+              onClick={() => {
+                if (this.props.auth) {
+                  console.log(this.props.currentCountry);
+                  fetch('/playlist/save', {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    credentials: 'include',
+                    body: JSON.stringify({
+                      country: this.props.currentCountry,
+                      tracks: this.props.playlist,
+                    }),
+                  })
+                  .then(res => console.log(res))
+                  .catch(err => console.log(err));
+                }
+              }
+            }
+            />
             <i className="fa fa fa-step-backward fa-lg fa-fw" />
             <i
               className={`fa fa-${playIcon} fa-2x fa-fw`}
