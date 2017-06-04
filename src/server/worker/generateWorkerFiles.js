b0fefb9ad63fc1f23d191f6770c7af79bfd01dcf
Arturo Ruvalcaba <a.ruvham@gmail.com>
2017-06-04 13:03:53 -0400

    [feat] generateWorketFiles.js fully functional

--- src/server/worker/generateWorkerFiles.js
+++ src/server/worker/generateWorkerFiles.js
@@ -5,10 +5,11 @@ const startingTime = Date.now();
 const token = 'BQBWZhesV_ne2ToC1AAJ_t5XxTVL3w9ksRifw4uLVACnvXqalAAQZE7GVrWFJQHIqG0ZwPeKhleIMqspBnjbzmy9AExDE8nJP1JvdzN_yyLfOVuShc84ekg96CXvP7E7zeJn_02Njbh8KgSRSYjZA3ojcMIOlQ6P5BZ-Ymytc-2FrTriQ-vqgEKeXUvwERF-dyI0qYSOOiuwi2MBB6xdoqT2ayaO_VsrkukcJoA-xAAHEfI8FbRhDgBQMDC5_UkMttXIMa9jrmiZEA3LBQ-ZT-IAE7dbwIof4oGT8W2N1g3TkGNO-VSJQzrtVqt309NadO9poA';
 const owner = 'thesoundsofspotify';
 const startingIndex = 0;// 3398;
-const endingIndex = 10;// 4928
+const endingIndex = 5000;// 4928
 const promises = [];
 let currentPlaylistType = 'COUNTRY';
 let currentPlaylistIndex = startingIndex;
+let endingFlag = null;
 const playlistIndexData = { startingIndexCountries: 0 };
 
 const getPlaylistInfo = (offset, limit) => {
@@ -23,21 +24,22 @@ const getPlaylistInfo = (offset, limit) => {
         currentPlaylistIndex++;
         switch (playlist.name) {
           case 'The Sound of \'s-hertogenbosch NL':
-            playlistIndexData.endingIndexCountries = currentPlaylistIndex - 1;
+            playlistIndexData.endingIndexCountries = currentPlaylistIndex - 2;
             playlistIndexData.startingIndexCities = currentPlaylistIndex;
             currentPlaylistType = 'CITY';
             break;
-          case 'The Sound of Cities':
+          case 'The Sounds of Cities':
             playlistIndexData.endingIndexCities = currentPlaylistIndex - 1;
             playlistIndexData.startingIndexGenres = currentPlaylistIndex + 1;
             currentPlaylistType = 'GENRE';
             break;
           case 'The Sound of Zydeco':
             playlistIndexData.endingIndexCities = currentPlaylistIndex;
-            currentPlaylistType = null;
+            endingFlag = currentPlaylistIndex + 1;
             break;
           default: break;
         }
+        if (currentPlaylistIndex > endingIndex || currentPlaylistIndex === endingFlag) currentPlaylistType = 'DELETE';
         return {
           name: `[${currentPlaylistType} - ${currentPlaylistIndex}] ${playlist.name}`,
           id: playlist.id,
@@ -45,11 +47,11 @@ const getPlaylistInfo = (offset, limit) => {
         };
       });
     })
+    .then(res => res.filter(p => !p.name.includes('DELETE')))
     .then((res) => {
       const string = JSON.stringify(res);
-      const parsedString = `${string.split('').splice(1, string.length - 2).join('')},`;
-      fs.appendFile('./playlistData.js', parsedString,
-      (err) => {
+      const parsedString = `${string.split('').splice(1, string.length - 2).join('')}${string.length > 2 ? ',' : ''}`;
+      fs.appendFile('./playlistData.js', parsedString, (err) => {
         if (err) reject(err);
         else resolve();
       });
@@ -61,7 +63,7 @@ const getPlaylistInfo = (offset, limit) => {
 const getPlaylistInfoGenerator = (offset, limit) => () => getPlaylistInfo(offset, limit);
 
 promises.push(() => new Promise((resolve, reject) => {
-  fs.writeFile('./playlistData.js', 'const playistData = [',
+  fs.writeFile('./playlistData.js', 'const playlistData = [',
   (err) => {
     if (err) reject(err);
     else resolve();
@@ -73,7 +75,7 @@ for (let i = startingIndex; i < endingIndex; i += 50) {
 }
 
 promises.push(() => new Promise((resolve, reject) => {
-  fs.appendFile('./playlistData.js', ']; module.exports = playistData;',
+  fs.appendFile('./playlistData.js', ']; module.exports = playlistData;',
   (err) => {
     if (err) reject(err);
     else {
@@ -84,7 +86,7 @@ promises.push(() => new Promise((resolve, reject) => {
 }));
 
 promises.push(() => new Promise((resolve, reject) => {
-  fs.writeFile('./playlistIndexData.js', 'const playistIndexData = ',
+  fs.writeFile('./playlistIndexData.js', 'const playlistIndexData = ',
   (err) => {
     if (err) reject(err);
     else resolve();
