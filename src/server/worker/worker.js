f25eb7d1b66217c632df44497e03a62a9ff7be41
Arturo Ruvalcaba <a.ruvham@gmail.com>
2017-06-05 13:31:39 -0400

    [feat] make auth automatic for worker

--- src/server/worker/worker.js
+++ src/server/worker/worker.js
@@ -3,12 +3,28 @@ const knex = require('../db/db');
 const playlistData = require('./playlistData.js');
 const playlistIndexData = require('./playlistIndexData.js');
 const playlistExceptions = require('./playlistExceptions.js');
+const config = require('../../../config');
 
 const TRACK_DEBUG = true;
 const startingTime = Date.now();
-const token = 'BQBlUUxr-DSo1GGTaa3ixX3LKuWsPptu9B2yQ00QuqZkjpX9JeFqjHyGcVY1QFnITbUkLr5u0nukjDcLIsZBmnF3caOnT3hy_xBEUkDKd_xc1XYYAFxASfch0ljbjxBLCTwO5qicfe7_Byzql-gkWnMk_HXosL9MxS0UoqLghTzz3oACjK0D-spHEW9jv3yM33mX_AowSPuMwTW5kwwehCr5kwydyUzjWn65zghIMpX_yG_s_-tFqtvS8WijHa48dbgHmbvPRDwvAtuNIh1R_fLWc1-Ab3VQTXaBSfAxZJiNXcXf_ohi98Hz0tNVYJgRQPyjlQ';
 const owner = 'thesoundsofspotify';
 const maxTracksPerPlaylist = 200;
+
+const getAuth = () =>
+  new Promise((resolve, reject) => {
+    const options = {
+      url: 'https://accounts.spotify.com/api/token',
+      headers: { Authorization: 'Basic ' + (new Buffer(config.clientID + ':' + config.clientSecret).toString('base64')) },
+      form: { grant_type: 'client_credentials' },
+      json: true,
+    };
+    request.post(options)
+    .then((res) => {
+      resolve(res.access_token);
+    })
+    .catch(err => reject(err));
+  });
+
 
 const savePlaylistInDatabase = playlist =>
   new Promise((resolve, reject) => {
@@ -54,36 +70,40 @@ const saveTrackInDatabaseGenerator = track => () => saveTrackInDatabase(track);
 
 const getPlaylistTrackData = (id, offset, limit) =>
   new Promise((resolve, reject) => {
-    const fields = 'items(track(album(type,name,id,images),artists(name,id),available_markets,duration_ms,name,id,preview_url,track_number,popularity))';
-    request(`https://api.spotify.com/v1/users/${owner}/playlists/${id}/tracks?fields=${fields}&limit=${limit}&offset=${offset}`,
-    { headers: { Authorization: `Bearer ${token}` } })
-    .then(res => JSON.parse(res))
-    .then(res => res.items)
-    .then(res => res.map(t => t.track))
-    .then((res) => {
-      return res.map((t) => {
-        if (t === null) return null;
-        else if (!t.available_markets.includes('US')) return null;
-        return {
-          track_id: t.id,
-          track_artist_id: JSON.stringify(t.artists.map(artist => artist.id)),
-          track_artist_name: JSON.stringify(t.artists.map(artist => artist.name)),
-          track_name: t.name,
-          track_preview_url: t.preview_url !== undefined ? t.preview_url : null,
-          track_album_id: t.album.id,
-          track_album_name: t.album.name,
-          track_album_type: t.album.type,
-          track_album_image: t.album.images[0] !== undefined ? t.album.images[0].url : null,
-          track_popularity: t.popularity,
-          track_length: t.duration_ms,
-          track_position: t.track_number,
-        };
-      });
+    getAuth()
+    .then(token => {
+      const fields = 'items(track(album(type,name,id,images),artists(name,id),available_markets,duration_ms,name,id,preview_url,track_number,popularity))';
+      request(`https://api.spotify.com/v1/users/${owner}/playlists/${id}/tracks?fields=${fields}&limit=${limit}&offset=${offset}`,
+      { headers: { Authorization: `Bearer ${token}` } })
+      .then(res => JSON.parse(res))
+      .then(res => res.items)
+      .then(res => res.map(t => t.track))
+      .then((res) => {
+        return res.map((t) => {
+          if (t === null) return null;
+          else if (!t.available_markets.includes('US')) return null;
+          return {
+            track_id: t.id,
+            track_artist_id: JSON.stringify(t.artists.map(artist => artist.id)),
+            track_artist_name: JSON.stringify(t.artists.map(artist => artist.name)),
+            track_name: t.name,
+            track_preview_url: t.preview_url !== undefined ? t.preview_url : null,
+            track_album_id: t.album.id,
+            track_album_name: t.album.name,
+            track_album_type: t.album.type,
+            track_album_image: t.album.images[0] !== undefined ? t.album.images[0].url : null,
+            track_popularity: t.popularity,
+            track_length: t.duration_ms,
+            track_position: t.track_number,
+          };
+        });
+      })
+      .then(res =>
+        res.filter(t => t !== null && t.track_album_image !== null && t.preview_url !== null))
+      .then(res => resolve(res))
+      .catch(err => reject(err));
     })
-    .then(res =>
-      res.filter(t => t !== null && t.track_album_image !== null && t.preview_url !== null))
-    .then(res => resolve(res))
-    .catch(err => reject(err));
+    .catch(err => console.log(err));
   });
 
 const getPlaylistTrackDataGenerator = (id, offset) => () => getPlaylistTrackData(id, offset, 100);
@@ -172,7 +192,7 @@ const getMultiplePlaylists = playlistBatch =>
 // .catch(err => console.log(err));
 
 // GET GENRES
-getMultiplePlaylists(playlistData.splice(4530))
+getMultiplePlaylists(playlistData.splice(4327))
 .then(() => {
   console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
   console.log(`[${Date.now() - startingTime}ms] WORKER SUCCESSFUL!`);
