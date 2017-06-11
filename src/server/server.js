3315bca55f2b9ed557f7fff05644ee143ed01987
Arturo Ruvalcaba <a.ruvham@gmail.com>
2017-06-10 22:11:41 -0400

    [update] fix bug with env variables, switched spotify client id

--- src/server/server.js
+++ src/server/server.js
@@ -14,7 +14,7 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(session({
-  secret: config.seshSecret,
+  secret: process.env.seshSecret,
   resave: false,
   saveUninitialized: false,
 }));
