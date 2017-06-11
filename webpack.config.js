3ca528d870e72c064eb773964f1dcb7b9ca1c20d
Arturo Ruvalcaba <a.ruvham@gmail.com>
2017-06-10 22:56:14 -0400

    [update] optimize webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const webpack = require('webpack');
+const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = {
+  devtool: 'cheap-module-source-map',
   entry: `${__dirname}/src/public/app/index.js`,
   output: {
     path: `${__dirname}/src/public/dist`,
@@ -22,8 +24,19 @@ module.exports = {
       },
     ],
   },
-  // plugins: [
-  //   new webpack.optimize.UglifyJsPlugin(),
-  //   new webpack.optimize.AggressiveMergingPlugin(),
-  // ],
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin(),
+    new CompressionPlugin({
+      asset: "[path].gz[query]",
+      algorithm: "gzip",
+      test: /\.js$|\.css$|\.html$/,
+      threshold: 10240,
+      minRatio: 0.8
+    }),
+  ],
 };
