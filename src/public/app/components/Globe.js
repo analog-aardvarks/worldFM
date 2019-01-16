babad6061c71f1a1d36b136c23a1d9f1462779a3
David Hamberlin <david.hamberlin.cft@jcrew.com>
2019-01-16 18:18:29 -0500

    Render globe from component, remove d3 script tag

--- src/public/app/components/Globe.js
+++ src/public/app/components/Globe.js
@@ -1,14 +1,22 @@
 import React, { PureComponent } from 'react'
+import * as d3 from 'd3'
 
-import renderGlobe from '../containers/renderGlobe'
+// import renderGlobe from '../containers/renderGlobe'
 
 import topoJSON from '../../assets/topoJSON'
-import countrieNames from '../../assets/countryNames'
+import availableCountries from '../constants/availableCountries';
+import countriesByID from '../../assets/countriesByID';
 
+const globeConfig = {
+  width: 400,
+  height: 400,
+  sens: 0.25,
+  startingRotation: [0, -11]
+}
 class Globe extends PureComponent {
   componentDidMount() {
     // Initiate spin, set event listeners
-    this.globe = renderGlobe(this.container, [-100, 0]);
+    this.setupGlobe()
   }
 
   componentWillUnmount() {
@@ -17,6 +25,53 @@ class Globe extends PureComponent {
 
   setupGlobe() {
     // initialize d3 code
+    const {
+      width,
+      height,
+      sens,
+      startingRotation,
+    } = globeConfig;
+
+    this.globe = {};
+
+    // Set projection
+    const projection = d3.geoOrthographic()
+      .scale(height / 2)
+      .rotate(startingRotation)
+      .translate([width / 2, height / 2])
+      .clipAngle(90);
+
+    const path = d3.geoPath().projection(projection)
+
+    this.svg = d3.select(this.container).append('svg')
+      .attr('width', width)
+      .attr('height', height)
+      .attr('class', 'globe');
+
+    // Add water
+    this.svg.append('path')
+      .datum({ type: 'Sphere' })
+      .attr('class', 'water')
+      .attr('d', path);
+
+    // const globeSelect = d3.select(this.container)
+    //   .append('select')
+    //   .attr('class', 'globeSelect')
+    //   .attr('name', 'countries');
+    // TODO add countries to select
+
+
+    // Add land
+    const countries = topojson.feature(topoJSON, topoJSON.objects.countries).features;
+
+
+    const land = this.svg.selectAll('path.land')
+      .data(countries)
+      .enter().append('path')
+      .attr('class', 'land')
+      .attr('d', path)
+      .filter(d => availableCountries.includes(countriesByID[d.id]))
+        .classed('available', true);
   }
 
   updateGlobe() {
